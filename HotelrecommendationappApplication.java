005c828ddaf4b1d08a32cba651a2c92fa8c279dd
Abhijit-Chavan07 <abhijitc386@gmail.com>
2025-04-19 13:22:14 +0530

    Add files via upload

---
+++ HotelrecommendationappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.hotelrecommendationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelrecommendationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelrecommendationappApplication.class, args);
+	}
+
+}
